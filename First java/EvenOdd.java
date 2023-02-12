c6ea835931eb482b8d1258cba15d51a75106c9f2
SaswatPal <108214921+SaswatPal@users.noreply.github.com>
2023-02-12 17:01:37 +0530

    Update EvenOdd.java

--- First java/EvenOdd.java
+++ First java/EvenOdd.java
@@ -1,5 +1,5 @@
 
-//Write a program to print whether a number is even or odd, also take input from the user.//
+// 1. Write a program to print whether a number is even or odd, also take input from the user.//
 
 import java.util.Scanner;
 
