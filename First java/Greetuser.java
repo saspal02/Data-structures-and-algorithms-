b45a280e4e6acd075c6848946dd302190bc906e6
SaswatPal <108214921+SaswatPal@users.noreply.github.com>
2023-02-12 17:04:46 +0530

    Add files via upload

---
+++ First java/Greetuser.java
@@ -0,0 +1,16 @@
+//Take name as input and print a greeting message for that particular name.
+
+import java.util.Scanner;
+
+public class Greetuser {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name + ", welcome to the world of Java programming!");
+    }
+}
+
+
