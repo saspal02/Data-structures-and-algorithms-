c50832fcde5308184693e3cd84bbef827feff8be
SaswatPal <108214921+SaswatPal@users.noreply.github.com>
2023-02-12 17:05:07 +0530

    Update Greetuser.java

--- First java/Greetuser.java
+++ First java/Greetuser.java
@@ -1,4 +1,4 @@
-//Take name as input and print a greeting message for that particular name.
+// 2. Take name as input and print a greeting message for that particular name.
 
 import java.util.Scanner;
 
