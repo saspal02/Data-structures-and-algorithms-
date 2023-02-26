66e8525ee1715602889c083f785b0d7ea2f9654f
Saswat Pal <108214921+SaswatPal@users.noreply.github.com>
2023-02-26 22:21:59 +0530

    Add files via upload

---
+++ conditional-loops/Basic java/PerimeterOfRhombus.java
@@ -0,0 +1,8 @@
+// 13.Perimeter Of Rhombus
+public class PerimeterOfRhombus {
+    public static void main(String[] args) {
+        int side = 5;
+        int perimeter = 4 * side;
+        System.out.println("Perimeter of rhombus with side " + side + " is " + perimeter);
+    }
+}
