10e210b421083194beaf190c3ab6598889050df0
Saswat Pal <saswat2001@gmail.com>
2024-07-10 13:31:32 +0530

    Add Collections

--- OOP/Collections/Main.java
+++ OOP/Collections/Main.java
@@ -30,6 +30,7 @@ public class Main {
         vector.add("5");
         vector.add("10");
         vector.add("15");
+
         System.out.println("Vector " + vector);
 
         //Stack
@@ -38,9 +39,105 @@ public class Main {
         stack.push(20);
         stack.push(30);
         stack.pop();
+
         System.out.println("Stack: "+ stack);
 
-        /pakist
+        //Queue
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.offer(20);
+        queue.offer(15);
+        queue.add(50);
+
+        System.out.println("Queue: "+ queue);
+        System.out.println("Queue remove: "+queue.remove());
+        System.out.println("New Queue: "+queue);
+        System.out.println("Queue Poll: "+queue.poll());
+        System.out.println("Queue: "+queue);
+        System.out.println("Queue Peek: "+queue.peek());
+
+        //PriorityQueue
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.offer(12);
+        pq.add(3);
+        pq.add(4);
+        pq.offer(5);
+        System.out.println("PriorityQueue: "+ pq.peek());
+
+        //DeQueue
+        Deque<Integer> deQueue = new ArrayDeque<Integer>();
+        deQueue.offer(10);
+        deQueue.offer(20);
+        deQueue.offerLast(30);
+        deQueue.offerFirst(40);
+        deQueue.pollLast();
+
+        System.out.println("Deque: "+ deQueue);
+
+        //Set
+        //HashSet
+        Set<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(40);
+        set.add(50);
+
+        System.out.println("HashSet: " + set);
+
+        //LinkedHashSet
+        LinkedHashSet<Integer> llset = new LinkedHashSet<>();
+
+        llset.add(10);
+        llset.add(20);
+        llset.add(30);
+        llset.add(15);
+
+        System.out.println("LinkedHashSet: " + llset);
+
+        //TreeSet
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(20);
+        treeSet.add(5);
+        treeSet.add(16);
+        treeSet.add(40);
+
+        System.out.println("TreeSet: "+ treeSet);
+
+        //Map
+        //HashMap
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("Second",30);
+        map.put("First",5);
+        System.out.println("HashMap: "+map);
+
+        //TreeMap
+        TreeMap<String,Integer> tMap = new TreeMap<>();
+        tMap.put("First",20);
+        tMap.put("Second",5);
+        System.out.println("TreeMap: "+tMap);
+        System.out.println(tMap.get("First"));
+        //Key
+        System.out.println(tMap.keySet());
+        //Value
+        System.out.println(tMap.values());
+
+        //Collections
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(2);
+        myList.add(4);
+        myList.add(1);
+        myList.add(0);
+
+        Collections.sort(myList);
+        System.out.println("Array Sorted by collections: "+myList);
+
+        //Iterator
+        for (Integer integer : myList) {
+            System.out.println("List value: " + integer);
+
+        }
+
 
 
 
