ec6027cf9fee3d28e20b2ddf227fdce9c50bd876
Saswat Pal <saswat2001@gmail.com>
2024-08-22 21:33:51 +0530

    Add Selection sort

---
+++ Sorting/SelectionSort/Main.java
@@ -0,0 +1,40 @@
+package SelectionSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {-6,1,5,4,2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void selection(int[] arr) {
+        //find the max item in the remaining array and swap with correct index
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
+
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+
+        }
+        return max;
+    }
+}
